fbac3e491e0c2e8238422aadf10675e19374f855
lijianyu <976568355@qq.com>
2020-01-06 20:22:16 +0800

    版本1

---
+++ demo/src/main/java/com/unit/demo/hello.java
@@ -0,0 +1,11 @@
+package com.unit.demo;
+
+public class hello {
+
+    static class inner {
+
+        public static void main(String[] args) {
+            System.out.println(123);
+        }
+    }
+}
